8877f14fbd4b94b210096f5f6983e384dc977052
Makash147 <102426802+Makash147@users.noreply.github.com>
2024-08-15 10:11:05 +0530

    Delete notifyMe/DesktopObserver.cpp

--- notifyMe/DesktopObserver.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "DesktopObserver.h"
-
-DesktopObserver::DesktopObserver(const std::string desktopName, EarPhoneObservable earPhoneObservable) : desktopName(desktopName), earPhoneObservable(earPhoneObservable) {}
-
-void DesktopObserver::sendNotification() {
-	std::cout << "Desktop notified." << std::endl;
-}
