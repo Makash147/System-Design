99e7216a2001743f59f310d2da3afdcbc35603ca
Makash147 <102426802+Makash147@users.noreply.github.com>
2024-08-15 10:11:22 +0530

    Delete notifyMe/MobileObserver.cpp

--- notifyMe/MobileObserver.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "MobileObserver.h"
-
-MobileObserver::MobileObserver(const std::string& mobileName, EarPhoneObservable& earPhoneObservable) : mobileName(mobileName), earPhoneObservable(earPhoneObservable) {}
-
-void MobileObserver::sendNotification() {
-	std::cout << "Mobile notified." << std::endl;
-}
